f54862ff7cf1739c00f1d64933c99bb9270cef7d
apfesta <andy_festa@yahoo.com>
2020-04-30 08:51:53 -0400

    started css and js

--- src/main/java/com/andrewfesta/doublesolitare/controller/MainController.java
+++ src/main/java/com/andrewfesta/doublesolitare/controller/MainController.java
@@ -1,11 +1,21 @@
 package com.andrewfesta.doublesolitare.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.andrewfesta.doublesolitare.model.GameBoard;
 
 @Controller
 public class MainController {
+	
+	Map<Integer, GameBoard> games = new HashMap<>();
+	AtomicInteger gameIdSequence = new AtomicInteger(0);
 
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String displayBoard() {
@@ -20,6 +30,17 @@ public class MainController {
 	
 	//Move card to foundation
 	
+	@RequestMapping(value="/api/game", method = RequestMethod.POST)
+	public GameBoard newGame() {
+		GameBoard game = new GameBoard(gameIdSequence.incrementAndGet());
+		game.setup();
+		games.put(game.getGameId(), game);
+		return game;
+	}
 	
+	@RequestMapping(value="/api/game/{gameId}", method = RequestMethod.GET)
+	public GameBoard getGame(@RequestParam Integer gameId) {
+		return games.get(gameId);
+	}
 	
 }
--- src/main/java/com/andrewfesta/doublesolitare/model/GameBoard.java
+++ src/main/java/com/andrewfesta/doublesolitare/model/GameBoard.java
@@ -2,16 +2,26 @@ package com.andrewfesta.doublesolitare.model;
 
 public class GameBoard {
 
+	final Integer gameId;
 	Tableau tableau;
 	Pile stockPile;
 	Foundation foundation;
 	
+	public GameBoard(Integer gameId) {
+		super();
+		this.gameId = gameId;
+	}
+
 	public void setup() {
 		tableau = new Tableau();
 		stockPile = tableau.setup();
 		foundation = new Foundation();
 		
 		tableau.prettyPrint();
+	}
+
+	public Integer getGameId() {
+		return gameId;
 	}
 	
 }
