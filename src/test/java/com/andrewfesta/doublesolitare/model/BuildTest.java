908cd01348f84f0fc214ad17210ab697a88aec44
apfesta <andy_festa@yahoo.com>
2020-05-13 00:35:59 -0400

    fixed junit test

--- src/test/java/com/andrewfesta/doublesolitare/model/BuildTest.java
+++ src/test/java/com/andrewfesta/doublesolitare/model/BuildTest.java
@@ -16,8 +16,8 @@ public class BuildTest {
 	public void testCanPushAlternateColor() {
 		Card c1 = new Card(10, Suit.HEARTS);
 		Build build = new Build(Sequence.ALTERNATE_COLOR);
-		build.push(c1);
-		
+		build.cards.push(c1);
+				
 		Card c2 = new Card(9, Suit.CLUBS);
 		Card c3 = new Card(9, Suit.HEARTS); //wrong color
 		Card c4 = new Card(Card.JACK, Suit.SPADES); //out of sequence
@@ -30,20 +30,25 @@ public class BuildTest {
 		assertFalse(build.canPush(c5));
 		assertFalse(build.canPush(c6));
 		
+		//In order to actually push the card must already be on a build or pile
+		Pile pile = new Pile();
+		c2.setCurrentPile(pile);
+		pile.push(c2); 
+		
 		build.push(c2);
 	}
 	
 	@Test
 	public void testCanPushBuilds() {
 		Build build1 = new Build(Sequence.ALTERNATE_COLOR);
-		build1.push(new Card(10, Suit.HEARTS));
-		build1.push(new Card(9, Suit.CLUBS));
+		build1.cards.push(new Card(10, Suit.HEARTS));
+		build1.cards.push(new Card(9, Suit.CLUBS));
 		
 		Build build2 = new Build(Sequence.ALTERNATE_COLOR);
-		build2.push(new Card(Card.JACK, Suit.SPADES));
+		build2.cards.push(new Card(Card.JACK, Suit.SPADES));
 		
 		Build build3 = new Build(Sequence.ALTERNATE_COLOR);
-		build3.push(new Card(8, Suit.DIAMONDS));
+		build3.cards.push(new Card(8, Suit.DIAMONDS));
 		
 		assertTrue(build2.canPush(build1));
 		assertFalse(build1.canPush(build2));
@@ -59,6 +64,12 @@ public class BuildTest {
 		Card c2 = new Card(2, Suit.HEARTS); 
 		assertTrue(build.canPush(c1));
 		assertFalse(build.canPush(c2)); //Not ACE
+		
+		//In order to actually push the card must already be on a build or pile
+		Pile pile = new Pile();
+		c1.setCurrentPile(pile);
+		pile.push(c1); 
+		
 		build.push(c1);
 				
 		//Stack next		
