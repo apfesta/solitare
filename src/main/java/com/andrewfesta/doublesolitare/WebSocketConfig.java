8abde21f158f453996f29792ab10710c76a27e32
apfesta <andy_festa@yahoo.com>
2020-05-19 09:37:42 -0400

    Create WebSocketConfig.java

---
+++ src/main/java/com/andrewfesta/doublesolitare/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.andrewfesta.doublesolitare;
+
+//@Configuration
+//@EnableWebSocketMessageBroker
+public class WebSocketConfig {
+  //implements WebSocketMessageBrokerConfigurer
+  
+  //public void registerStompEndpoints(StompEndpointRegistery registry) {
+  //  register.addEndpoint("/stomp").withSockJS();
+  //}
+  
+ // public void configureMessageBroker(MessageBrokerRegistrry registry) {
+  //  registry.enableSimpleBroker("/topic");
+  //  registry.setApplicationDestinationPrefixes("/app");
+  //}
+}
